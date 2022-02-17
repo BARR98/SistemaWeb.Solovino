852200a8d543205a0c2a743621149fdc8fddefda
Brayan <brayanrodriguezrmz98@gmail.com>
2022-02-16 22:18:46 -0600

    Inicio del repositorio y subiendo el proyecto

---
+++ src/frontend/src/app/public/public-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdopcionComponent } from './components/adopcion/adopcion.component';
+import { HomePublicComponentComponent } from './components/home-public-component/home-public-component.component';
+import { PublicDonacionesComponent } from './components/public-donaciones/public-donaciones.component';
+import { SobreNostrosComponent } from './components/sobre-nostros/sobre-nostros.component';
+
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomePublicComponentComponent
+
+  }, 
+  {
+    path:'adopcion',
+    component: AdopcionComponent
+
+  }, 
+  {
+    path:'sobre-nostros',
+    component: SobreNostrosComponent
+
+  },   
+  {
+    path:'donaciones',
+    component: PublicDonacionesComponent
+
+  },   
+  {
+    path:'',
+    component: HomePublicComponentComponent
+
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PublicRoutingModule { }
